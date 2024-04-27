90df51d29d22f3f315625f6704734edd1e624d8c
Rubal <rubal.goyal@outlook.com>
2024-04-27 14:05:46 -0600

    Fixed HW5, buttons working fine now.

--- Lexi copy/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-import javax.naming.OperationNotSupportedException;
-
-public class Main {
-    public static void main(String[] args) throws OperationNotSupportedException {
-        Tests.hw2tests();
-    }
-}
-
--- Lexi copy/src/glyph/Compositor.java
+++
@@ -1,9 +0,0 @@
-package glyph;
-
-/*
-This is an interface to implement Strategy Design Pattern for Formatting the lexi document.
- */
-interface Compositor {
-    void compose();
-    void setComposition(Composition composition);
-}
